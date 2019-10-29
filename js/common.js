15d22818e90966f3ab8307d854857421dbc71f5f
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-10-29 21:28:14 +0300

    c

--- js/common.js
+++ js/common.js
@@ -1,10 +1,10 @@
 // ========================>
 // navigation
 // <========================
-function destop() {
+(function () {
 
-  let sect_main = document.querySelector('.main')
-  let mainContent = sect_main.querySelector('.main_content')
+  let sect_main = document.querySelector('.main');
+  let mainContent = sect_main.querySelector('.main_content');
   let control = sect_main.querySelectorAll('.main-menu__btn');
 
   let sect_tabs = document.querySelector('.about');
@@ -13,29 +13,44 @@ function destop() {
 
   function hideAll(elems) {
     elems.forEach(el => {
-      el.classList.remove('active')
-    })
+      el.classList.remove('active');
+    });
   };
   function addActiveItem(i) {
     hideAll(tabs);
     hideAll(tabsContent);
-    tabsContent[i].classList.add('active')
-    tabs[i].classList.add('active')
+    tabsContent[i].classList.add('active');
+    tabs[i].classList.add('active');
   }
   tabs.forEach((item, i) => {
     item.addEventListener('click', () => {
-      addActiveItem(i);
+      if (item.classList.contains('active')) {
+        hideAll(tabs);
+        hideAll(tabsContent);
+      }
+      else addActiveItem(i);
     })
   });
   control.forEach((item, i) => {
     item.addEventListener('click', () => {
       addActiveItem(i);
-      mainContent.classList.add('active')
+      mainContent.classList.add('active');
       setTimeout(() => {
-        sect_main.style.opacity = '0'
-        sect_tabs.style.display = 'block'
-      }, 1300);
+        sect_main.style.opacity = '0';
+        setTimeout(() => {
+          sect_tabs.style.display = 'block';
+        }, 200);
+      }, 400);
     })
   })
-};
-destop()
+})();
+
+if (document.documentElement.clientWidth < 560) scrollToTab()
+
+function scrollToTab() {
+  let btns = document.querySelectorAll('.tabs__item-list')
+  btns.forEach(btn => {
+    console.log(btn)
+  })
+}
+
