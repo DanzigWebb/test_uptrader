b39298cf86c6a873a793db780ba4cceac3bb14ad
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-10-29 19:09:14 +0300

    с

--- js/common.js
+++ js/common.js
@@ -1,8 +1,41 @@
-(function(){
-  let screenItem = document.querySelectorAll('.f-screen__item');
-  let count = 0.5;
-  screenItem.forEach(item => {
-    item.style.animationDelay = count + 's'
-    count+= 0.2
+// ========================>
+// navigation
+// <========================
+function destop() {
+
+  let sect_main = document.querySelector('.main')
+  let mainContent = sect_main.querySelector('.main_content')
+  let control = sect_main.querySelectorAll('.main-menu__btn');
+
+  let sect_tabs = document.querySelector('.about');
+  let tabs = sect_tabs.querySelectorAll('.tabs__item');
+  let tabsContent = sect_tabs.querySelectorAll('.tabs-content');
+
+  function hideAll(elems) {
+    elems.forEach(el => {
+      el.classList.remove('active')
+    })
+  };
+  function addActiveItem(i) {
+    hideAll(tabs);
+    hideAll(tabsContent);
+    tabsContent[i].classList.add('active')
+    tabs[i].classList.add('active')
+  }
+  tabs.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      addActiveItem(i);
+    })
+  });
+  control.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      addActiveItem(i);
+      mainContent.classList.add('active')
+      setTimeout(() => {
+        sect_main.style.opacity = '0'
+        sect_tabs.style.display = 'block'
+      }, 1300);
+    })
   })
-})()
+};
+destop()
